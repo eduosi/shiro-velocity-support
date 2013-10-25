dacac4d64c6c8269f5758780b88728e5b162ef9b
yong.teng <webmaster@buession.com>
2013-10-26 01:59:45 +0800

    initialize

--- README.md
+++ README.md
@@ -1,2 +1,100 @@
-shiro-velocity-support
-======================
+velocity-shiro
+==============
+
+在 velocity 模板文件中，实现 shiro 权限验证
+在 applicationContext.xml 只有此功能的核心配置的例子，其它相关配置不属于此项目范围之类。
+velocityToolBox.xml 文件中也只列出了，该功能的配置。
+
+使用方法：
+
+	一、$shiro.isAuthenticated()
+		功能说明：验证是否为已认证通过的用户，不包含已记住的用户，这是与 isUser 标签方法的区别所在。
+		参数：无
+		返回值：Boolean
+
+	二、$shiro.isNotAuthenticated()
+		功能说明：验证是否为未认证通过用户，与 isAuthenticated 标签相对应，与 isGuest 标签的区别是，该标签包含已记住用户。
+		参数：无
+		返回值：Boolean
+
+	三、$shiro.isGuest()
+		功能说明：验证当前用户是否为“访客”，即未认证（包含未记住）的用户。
+		参数：无
+		返回值：Boolean
+
+	四、$shiro.isUser()
+		功能说明：验证当前用户是否认证通过或已记住的用户。
+		参数：无
+		返回值：Boolean
+
+	五、$shiro.getPrincipal()
+		功能说明：获取当前用户 Principal。
+		参数：无
+		返回值：Object
+
+	六、$shiro.getPrincipalProperty(String property)
+		功能说明：获取当前用户 Principal。
+		参数：String property 用户属性
+		返回值：Object
+
+	七、$shiro.hasRole(String role)
+		功能说明：验证当前用户是否属于该角色 。
+		参数：String role 角色名称
+		返回值：Boolean
+
+	八、$shiro.lacksRole(String role)
+		功能说明：验证当前用户是否不属于该角色，与 hasRole 标签逻辑相反。
+		参数：String role 角色名称
+		返回值：Boolean
+
+	九、$shiro.hasAnyRoles(String roleNames, String delimeter)
+		功能说明：验证当前用户是否属于以下任意一个角色。
+		参数：String roleNames 用户角色列表，以 delimeter 分割
+			  String delimeter 用户角色分隔符
+		返回值：Boolean
+
+	十、$shiro.hasAnyRoles(String roleNames)
+		功能说明：验证当前用户是否属于以下任意一个角色。
+		参数：String roleNames 用户角色列表，以 “," 分割
+		返回值：Boolean
+
+	十一、$shiro.hasAnyRoles(List<String> roleNames)
+		功能说明：验证当前用户是否属于以下任意一个角色。
+		参数：List<String> roleNames 用户权限角色
+		返回值：Boolean
+
+	十二、$shiro.hasAnyRoles(String[] roleNames)
+		功能说明：验证当前用户是否属于以下任意一个角色。
+		参数：String[] roleNames 用户权限角色
+		返回值：Boolean
+
+	十三、$shiro.hasPermission(String permission)
+		功能说明：验证当前用户是否拥有指定权限
+		参数：String permission 权限名称
+		返回值：Boolean
+
+	十四、$shiro.lacksPermission(String permission)
+		功能说明：验证当前用户是否不拥有指定权限，与 hasPermission 逻辑相反。
+		参数：String permission 权限名称
+		返回值：Boolean
+
+	十五、$shiro.hasAnyPermissions(String permissions, String delimeter)
+		功能说明：验证当前用户是否拥有以下任意一个权限。
+		参数：String permissions 权限名称列表，以 delimeter 分割
+			  String delimeter 用户权限分隔符
+		返回值：Boolean
+
+	十六、$shiro.hasAnyPermissions(String permissions)
+		功能说明：验证当前用户是否拥有以下任意一个权限。
+		参数：String permissions 权限名称列表，以 “," 分割
+		返回值：Boolean
+
+	十七、$shiro.hasAnyPermissions(List<String> permissions)
+		功能说明：验证当前用户是否拥有以下任意一个权限。
+		参数：List<String> 权限名称列表
+		返回值：Boolean
+
+	十八、$shiro.hasAnyPermissions(String[] permissions)
+		功能说明：验证当前用户是否拥有以下任意一个权限。
+		参数：String[] 权限名称列表
+		返回值：Boolean
---
+++ src/main/java/org/apache/shiro/web/support/velocity/Permission.java
@@ -0,0 +1,253 @@
+package org.apache.shiro.web.support.velocity;
+
+import java.beans.BeanInfo;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.Subject;
+import org.apache.velocity.tools.Scope;
+import org.apache.velocity.tools.config.DefaultKey;
+import org.apache.velocity.tools.config.ValidScope;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@DefaultKey("shiro")
+@ValidScope(Scope.REQUEST)
+public class Permission {
+
+	private static final String ROLE_NAMES_DELIMETER = ",";
+	private static final String PERMISSION_NAMES_DELIMETER = ",";
+
+	private static final Logger logger = LoggerFactory.getLogger(Permission.class);
+
+	protected HttpServletRequest request;
+	protected HttpServletResponse response;
+
+	public HttpServletRequest getRequest() {
+		return request;
+	}
+
+	public void setRequest(HttpServletRequest request) {
+		if (request == null) {
+			throw new NullPointerException("request should not be null");
+		}
+
+		this.request = request;
+	}
+
+	public HttpServletResponse getResponse() {
+		return response;
+	}
+
+	public void setResponse(HttpServletResponse response) {
+		this.response = response;
+	}
+
+	/* 验证是否为已认证通过的用户，不包含已记住的用户，这是与 isUser 标签方法的区别所在。 */
+	public boolean isAuthenticated() {
+		Subject subject = SecurityUtils.getSubject();
+		return subject != null && subject.isAuthenticated() == true;
+	}
+
+	/* 验证是否为未认证通过用户，与 isAuthenticated 标签相对应，与 isGuest 标签的区别是，该标签包含已记住用户。 */
+	public boolean isNotAuthenticated() {
+		Subject subject = SecurityUtils.getSubject();
+		return subject == null || subject.isAuthenticated() == false;
+	}
+
+	/* 验证当前用户是否为“访客”，即未认证（包含未记住）的用户。 */
+	public boolean isGuest() {
+		Subject subject = SecurityUtils.getSubject();
+		return subject == null || subject.getPrincipal() == null;
+	}
+
+	/* 验证当前用户是否认证通过或已记住的用户。 */
+	public boolean isUser() {
+		Subject subject = SecurityUtils.getSubject();
+		return subject != null && subject.getPrincipal() != null;
+	}
+
+	/* 获取当前用户 Principal。 */
+	public Object getPrincipal() {
+		Subject subject = SecurityUtils.getSubject();
+		if (subject != null) {
+			return subject.getPrincipal();
+		}
+
+		return null;
+	}
+
+	/* 获取当前用户属性。 */
+	public Object getPrincipalProperty(String property) {
+		Subject subject = SecurityUtils.getSubject();
+		Object value = null;
+
+		if (subject != null) {
+			Object principal = subject.getPrincipal();
+
+			try {
+				BeanInfo bi = Introspector.getBeanInfo(principal.getClass());
+
+				boolean foundProperty = false;
+				for (PropertyDescriptor pd : bi.getPropertyDescriptors()) {
+					if (pd.getName().equals(property)) {
+						value = pd.getReadMethod().invoke(principal, (Object[]) null);
+						foundProperty = true;
+						break;
+					}
+				}
+
+				if (!foundProperty) {
+					final String message = "Property [" + property
+							+ "] not found in principal of type [" + principal.getClass().getName()
+							+ "]";
+					logger.trace(message);
+				}
+			} catch (Exception e) {
+				final String message = "Error reading property [" + property
+						+ "] from principal of type [" + principal.getClass().getName() + "]";
+				logger.trace(message);
+			}
+		}
+
+		return value;
+	}
+
+	/* 验证当前用户是否属于该角色 。 */
+	public boolean hasRole(String role) {
+		Subject subject = SecurityUtils.getSubject();
+		return subject != null && subject.hasRole(role) == true;
+	}
+
+	/* 验证当前用户是否不属于该角色，与 hasRole 逻辑相反。 */
+	public boolean lacksRole(String role) {
+		return hasRole(role) != true;
+	}
+
+	/* 验证当前用户是否属于以下任意一个角色。 */
+	public boolean hasAnyRoles(String roleNames, String delimeter) {
+		Subject subject = SecurityUtils.getSubject();
+		if (subject != null) {
+			if (delimeter == null || delimeter.length() == 0) {
+				delimeter = ROLE_NAMES_DELIMETER;
+			}
+
+			for (String role : roleNames.split(delimeter)) {
+				if (subject.hasRole(role.trim()) == true) {
+					return true;
+				}
+			}
+		}
+
+		return false;
+	}
+
+	/* 验证当前用户是否属于以下任意一个角色。 */
+	public boolean hasAnyRoles(String roleNames) {
+		return hasAnyRoles(roleNames, ROLE_NAMES_DELIMETER);
+	}
+
+	/* 验证当前用户是否属于以下任意一个角色。 */
+	public boolean hasAnyRoles(List<String> roleNames) {
+		Subject subject = SecurityUtils.getSubject();
+
+		if (subject != null && roleNames != null) {
+			for (int i = 0; i < roleNames.size(); i++) {
+				String role = roleNames.get(i);
+				if (role != null && subject.hasRole(role.trim())) {
+					return true;
+				}
+			}
+		}
+
+		return false;
+	}
+
+	/* 验证当前用户是否属于以下任意一个角色。 */
+	public boolean hasAnyRoles(String[] roleNames) {
+		Subject subject = SecurityUtils.getSubject();
+
+		if (subject != null && roleNames != null) {
+			for (int i = 0; i < roleNames.length; i++) {
+				String role = roleNames[i];
+				if (role != null && subject.hasRole(role.trim())) {
+					return true;
+				}
+			}
+		}
+
+		return false;
+	}
+
+	/* 验证当前用户是否拥有指定权限 */
+	public boolean hasPermission(String permission) {
+		Subject subject = SecurityUtils.getSubject();
+		return subject != null && subject.isPermitted(permission);
+	}
+
+	/* 验证当前用户是否不拥有指定权限，与 hasPermission 逻辑相反。*/
+	public boolean lacksPermission(String permission) {
+		return hasPermission(permission) != true;
+	}
+
+	/* 验证当前用户是否拥有以下任意一个权限。 */
+	public boolean hasAnyPermissions(String permissions, String delimeter) {
+		Subject subject = SecurityUtils.getSubject();
+		if (subject != null) {
+			if (delimeter == null || delimeter.length() == 0) {
+				delimeter = PERMISSION_NAMES_DELIMETER;
+			}
+
+			for (String permission : permissions.split(delimeter)) {
+				if (subject.isPermitted(permission.trim()) == true) {
+					return true;
+				}
+			}
+		}
+
+		return false;
+	}
+
+	/* 验证当前用户是否拥有以下任意一个权限。 */
+	public boolean hasAnyPermissions(String permissions) {
+		return hasAnyPermissions(permissions, PERMISSION_NAMES_DELIMETER);
+	}
+
+	/* 验证当前用户是否拥有以下任意一个权限。 */
+	public boolean hasAnyPermissions(List<String> permissions) {
+		Subject subject = SecurityUtils.getSubject();
+
+		if (subject != null && permissions != null) {
+			for (int i = 0; i < permissions.size(); i++) {
+				String permission = permissions.get(i);
+				if (permission != null && subject.isPermitted(permission.trim())) {
+					return true;
+				}
+			}
+		}
+
+		return false;
+	}
+
+	/* 验证当前用户是否拥有以下任意一个权限。 */
+	public boolean hasAnyPermissions(String[] permissions) {
+		Subject subject = SecurityUtils.getSubject();
+
+		if (subject != null && permissions != null) {
+			for (int i = 0; i < permissions.length; i++) {
+				String permission = permissions[i];
+				if (permission != null && subject.isPermitted(permission.trim())) {
+					return true;
+				}
+			}
+		}
+
+		return false;
+	}
+
+}
