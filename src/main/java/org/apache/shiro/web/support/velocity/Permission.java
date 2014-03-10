b9ec684a818d8f9795e7c4a989005e71cf190aa4
yong.teng <eduosi@163.com>
2014-03-10 09:41:34 +0800

    modify copyright

--- src/main/java/org/apache/shiro/web/support/velocity/Permission.java
+++ src/main/java/org/apache/shiro/web/support/velocity/Permission.java
@@ -1,14 +1,12 @@
 /*
- * Copyright @ 2013-2013 yong.teng(webmaster@buession.com)
- * 
- * =================================================================================================
- * 
  * Licensed to the Apache Software Foundation (ASF) under one or more contributor license
  * agreements. See the NOTICE file distributed with this work for additional information regarding
  * copyright ownership. The ASF licenses this file to you under the Apache License, Version 2.0 
  * (the "License"); you may not use this file except in compliance with the License. You may obtain 
  * a copy of the License at
+ * 
  * http://www.apache.org/licenses/LICENSE-2.0
+ * 
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -21,11 +19,11 @@
  * <http://www.apache.org/>.
  * 
  * +------------------------------------------------------------------------------------------------+
- * | License: https://raw.github.com/eduosi/shiro-velocity-support/master/LICENSE 					|
+ * | License: License: https://mcrypt.buession.com.cn/LICENSE 										|
  * | Author: Yong.Teng <webmaster@buession.com> 													|
+ * | Copyright @ 2013-2014 Buession.com Inc.														|
  * +------------------------------------------------------------------------------------------------+
  */
-
 package org.apache.shiro.web.support.velocity;
 
 import java.beans.BeanInfo;
@@ -41,6 +39,11 @@ import org.apache.velocity.tools.config.ValidScope;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Velocity 模板引擎，Shiro 权限标签
+ * 
+ * @author Yong.Teng <webmaster@buession.com>
+ */
 @DefaultKey("shiro")
 @ValidScope(Scope.APPLICATION)
 public class Permission {
@@ -50,31 +53,51 @@ public class Permission {
 
 	private static final Logger logger = LoggerFactory.getLogger(Permission.class);
 
-	/* 验证是否为已认证通过的用户，不包含已记住的用户，这是与 isUser 标签方法的区别所在。 */
+	/**
+	 * 验证是否为已认证通过的用户，不包含已记住的用户，这是与 isUser 标签方法的区别所在。
+	 * 
+	 * @return 用户是否已通过认证
+	 */
 	public boolean isAuthenticated() {
 		Subject subject = SecurityUtils.getSubject();
 		return subject != null && subject.isAuthenticated() == true;
 	}
 
-	/* 验证是否为未认证通过用户，与 isAuthenticated 标签相对应，与 isGuest 标签的区别是，该标签包含已记住用户。 */
+	/**
+	 * 验证是否为未认证通过用户，与 isAuthenticated 标签相对应，与 isGuest 标签的区别是，该标签包含已记住用户。
+	 * 
+	 * @return 用户是否未通过认证
+	 */
 	public boolean isNotAuthenticated() {
 		Subject subject = SecurityUtils.getSubject();
 		return subject == null || subject.isAuthenticated() == false;
 	}
 
-	/* 验证当前用户是否为“访客”，即未认证（包含未记住）的用户。 */
+	/**
+	 * 验证用户是否为 <访客>，即未认证（包含未记住）的用户。
+	 * 
+	 * @return 用户是否为 <访客>
+	 */
 	public boolean isGuest() {
 		Subject subject = SecurityUtils.getSubject();
 		return subject == null || subject.getPrincipal() == null;
 	}
 
-	/* 验证当前用户是否认证通过或已记住的用户。 */
+	/**
+	 * 验证用户是否认证通过或已记住的用户。
+	 * 
+	 * @return 用户是否认证通过或已记住的用户
+	 */
 	public boolean isUser() {
 		Subject subject = SecurityUtils.getSubject();
 		return subject != null && subject.getPrincipal() != null;
 	}
 
-	/* 获取当前用户 Principal。 */
+	/**
+	 * 返回用户 Principal。
+	 * 
+	 * @return 用户 Principal
+	 */
 	public Object getPrincipal() {
 		Subject subject = SecurityUtils.getSubject();
 		if (subject != null) {
@@ -84,7 +107,13 @@ public class Permission {
 		return null;
 	}
 
-	/* 获取当前用户属性。 */
+	/**
+	 * 返回用户属性。
+	 * 
+	 * @param property
+	 *        属性名称
+	 * @return 用户属性
+	 */
 	public Object getPrincipalProperty(String property) {
 		Subject subject = SecurityUtils.getSubject();
 		Object value = null;
@@ -105,33 +134,50 @@ public class Permission {
 				}
 
 				if (!foundProperty) {
-					final String message = "Property [" + property
-							+ "] not found in principal of type [" + principal.getClass().getName()
-							+ "]";
-					logger.trace(message);
+					logger.trace("Property [{}] not found in principal of type [{}]", property,
+							principal.getClass().getName());
 				}
 			} catch (Exception e) {
-				final String message = "Error reading property [" + property
-						+ "] from principal of type [" + principal.getClass().getName() + "]";
-				logger.trace(message);
+				logger.trace("Error reading property [{}] from principal of type [{}]", property,
+						principal.getClass().getName());
 			}
 		}
 
 		return value;
 	}
 
-	/* 验证当前用户是否属于该角色 。 */
+	/**
+	 * 验证用户是否具备某角色。
+	 * 
+	 * @param role
+	 *        角色名称
+	 * @return 用户是否具备某角色
+	 */
 	public boolean hasRole(String role) {
 		Subject subject = SecurityUtils.getSubject();
 		return subject != null && subject.hasRole(role) == true;
 	}
 
-	/* 验证当前用户是否不属于该角色，与 hasRole 逻辑相反。 */
+	/**
+	 * 验证用户是否不具备某角色，与 hasRole 逻辑相反。
+	 * 
+	 * @param role
+	 *        角色名称
+	 * @return 用户是否不具备某角色
+	 */
 	public boolean lacksRole(String role) {
 		return hasRole(role) != true;
 	}
 
-	/* 验证当前用户是否属于以下任意一个角色。 */
+	/**
+	 * 验证用户是否具有以下任意一个角色。
+	 * 
+	 * @param roleNames
+	 *        以 delimeter 为分隔符的角色列表
+	 * @param delimeter
+	 *        角色列表分隔符
+	 * @return 用户是否具有以下任意一个角色
+	 */
 	public boolean hasAnyRoles(String roleNames, String delimeter) {
 		Subject subject = SecurityUtils.getSubject();
 		if (subject != null) {
@@ -149,12 +195,24 @@ public class Permission {
 		return false;
 	}
 
-	/* 验证当前用户是否属于以下任意一个角色。 */
+	/**
+	 * 验证用户是否具有以下任意一个角色。
+	 * 
+	 * @param roleNames
+	 *        以 ROLE_NAMES_DELIMETER 为分隔符的角色列表
+	 * @return 用户是否具有以下任意一个角色
+	 */
 	public boolean hasAnyRoles(String roleNames) {
 		return hasAnyRoles(roleNames, ROLE_NAMES_DELIMETER);
 	}
 
-	/* 验证当前用户是否属于以下任意一个角色。 */
+	/**
+	 * 验证用户是否具有以下任意一个角色。
+	 * 
+	 * @param roleNames
+	 *        角色列表
+	 * @return 用户是否具有以下任意一个角色
+	 */
 	public boolean hasAnyRoles(Collection<String> roleNames) {
 		Subject subject = SecurityUtils.getSubject();
 
@@ -169,7 +227,13 @@ public class Permission {
 		return false;
 	}
 
-	/* 验证当前用户是否属于以下任意一个角色。 */
+	/**
+	 * 验证用户是否具有以下任意一个角色。
+	 * 
+	 * @param roleNames
+	 *        角色列表
+	 * @return 用户是否具有以下任意一个角色
+	 */
 	public boolean hasAnyRoles(String[] roleNames) {
 		Subject subject = SecurityUtils.getSubject();
 
@@ -185,18 +249,38 @@ public class Permission {
 		return false;
 	}
 
-	/* 验证当前用户是否拥有指定权限 */
+	/**
+	 * 验证用户是否具备某权限。
+	 * 
+	 * @param permission
+	 *        权限名称
+	 * @return 用户是否具备某权限
+	 */
 	public boolean hasPermission(String permission) {
 		Subject subject = SecurityUtils.getSubject();
 		return subject != null && subject.isPermitted(permission);
 	}
 
-	/* 验证当前用户是否不拥有指定权限，与 hasPermission 逻辑相反。 */
+	/**
+	 * 验证用户是否不具备某权限，与 hasPermission 逻辑相反。
+	 * 
+	 * @param permission
+	 *        权限名称
+	 * @return 用户是否不具备某权限
+	 */
 	public boolean lacksPermission(String permission) {
 		return hasPermission(permission) != true;
 	}
 
-	/* 验证当前用户是否拥有以下任意一个权限。 */
+	/**
+	 * 验证用户是否具有以下任意一个权限。
+	 * 
+	 * @param permissions
+	 *        以 delimeter 为分隔符的权限列表
+	 * @param delimeter
+	 *        权限列表分隔符
+	 * @return 用户是否具有以下任意一个权限
+	 */
 	public boolean hasAnyPermissions(String permissions, String delimeter) {
 		Subject subject = SecurityUtils.getSubject();
 		if (subject != null) {
@@ -214,12 +298,24 @@ public class Permission {
 		return false;
 	}
 
-	/* 验证当前用户是否拥有以下任意一个权限。 */
+	/**
+	 * 验证用户是否具有以下任意一个权限。
+	 * 
+	 * @param permissions
+	 *        以 PERMISSION_NAMES_DELIMETER 为分隔符的权限列表
+	 * @return 用户是否具有以下任意一个权限
+	 */
 	public boolean hasAnyPermissions(String permissions) {
 		return hasAnyPermissions(permissions, PERMISSION_NAMES_DELIMETER);
 	}
 
-	/* 验证当前用户是否拥有以下任意一个权限。 */
+	/**
+	 * 验证用户是否具有以下任意一个权限。
+	 * 
+	 * @param permissions
+	 *        权限列表
+	 * @return 用户是否具有以下任意一个权限
+	 */
 	public boolean hasAnyPermissions(Collection<String> permissions) {
 		Subject subject = SecurityUtils.getSubject();
 
@@ -234,7 +330,13 @@ public class Permission {
 		return false;
 	}
 
-	/* 验证当前用户是否拥有以下任意一个权限。 */
+	/**
+	 * 验证用户是否具有以下任意一个权限。
+	 * 
+	 * @param permissions
+	 *        权限列表
+	 * @return 用户是否具有以下任意一个权限
+	 */
 	public boolean hasAnyPermissions(String[] permissions) {
 		Subject subject = SecurityUtils.getSubject();
 
